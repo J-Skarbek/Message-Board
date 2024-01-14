a5e28b0ff6e3731d546def932ad0692a4fc15c06
J-Skarbek <john@snarkmediaagency.com>
2024-01-14 11:38:55 -0600

    Create some sample messages and display them on index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const messages = [
+    {
+      text: "Hello!",
+      user: "Amanda",
+      added: new Date()
+    },
+    {
+      text: "Hello!",
+      user: "Josh",
+      added: new Date()
+    },
+    {
+      text: "Hello!",
+      user: "Adam",
+      added: new Date()
+    },
+    {
+      text: "Hello!",
+      user: "Kyle",
+      added: new Date()
+    },
+  ]
+
+
+  res.render('index', { title: 'Express', messages });
 });
 
 module.exports = router;
