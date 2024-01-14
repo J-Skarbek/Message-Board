9755787815fbd829c8e133f408c1874951ae4c84
J-Skarbek <john@snarkmediaagency.com>
2024-01-14 13:23:25 -0600

    Finish getting new messages to post to the array

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello!",
+    user: "Amanda",
+    added: new Date()
+  },
+  {
+    text: "Hello!",
+    user: "Josh",
+    added: new Date()
+  },
+  {
+    text: "Hello!",
+    user: "Adam",
+    added: new Date()
+  },
+  {
+    text: "Hello!",
+    user: "Kyle",
+    added: new Date()
+  },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages = [
-    {
-      text: "Hello!",
-      user: "Amanda",
-      added: new Date()
-    },
-    {
-      text: "Hello!",
-      user: "Josh",
-      added: new Date()
-    },
-    {
-      text: "Hello!",
-      user: "Adam",
-      added: new Date()
-    },
-    {
-      text: "Hello!",
-      user: "Kyle",
-      added: new Date()
-    },
-  ]
-
 
   res.render('index', { title: 'Mini Message Board', messages });
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user_name,
+    added: req.body.date
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
