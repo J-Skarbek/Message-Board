9c689d0fe3f8c4fc1f9cf047781b31972da3863d
J-Skarbek <john@snarkmediaagency.com>
2024-01-14 12:49:11 -0600

    Pull in tailwind via cdn; add form to its url

--- routes/new.js
+++ routes/new.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('form', { title: 'Add new' });
+  res.render('form', { title: 'New Message' });
 });
 
 module.exports = router;
